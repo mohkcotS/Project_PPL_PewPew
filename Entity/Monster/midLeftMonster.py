b97e56ef97db46a49a13974df7dedae5bb02aee4
= <phuongnguyen20904@gmail.com>
2025-05-08 14:34:38 +0700

    Feat: Add left and mid left monster

--- Entity/Monster/midLeftMonster.py
+++ Entity/Monster/midLeftMonster.py
@@ -2,13 +2,13 @@ from Entity.Monster.monster import Monster  # Import class Entity từ entity.py
 import pygame
 import math
 
-class MidRightMonster(Monster):  # Kế thừa class Entity
-    def __init__(self, x=600, y=0, direction="mid-right", color=(255, 255, 0)):
+class MidLeftMonster(Monster):  # Kế thừa class Entity
+    def __init__(self, x=200, y=0, direction="mid-left", color=(255, 255, 0)):
         super().__init__(x, y, direction, color)  
 
     def move(self):
         self.y += self.speed * math.sin(math.radians(67.3801))
-        self.x -= self.speed * math.cos(math.radians(67.3801))
+        self.x += self.speed * math.cos(math.radians(67.3801))
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
