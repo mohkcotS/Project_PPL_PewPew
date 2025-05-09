f0d5b17978990766ae63eb969edc8f2b32cb4153
Le Minh Duy <duylm2016@gmail.com>
2025-05-09 09:59:22 +0700

    feat: random monster

--- Screen/playScreen.py
+++ Screen/playScreen.py
@@ -6,61 +6,47 @@ from Entity.Monster.midMonster import MidMonster
 from Entity.Monster.leftMonster import LeftMonster
 from Entity.Monster.midLeftMonster import MidLeftMonster
 from Controller.keyhandler import KeyHandler
+import random
 
 def show_play_screen(screen,width,height,clock):
-    background = pygame.image.load("src/assets/Space_Background.png")
-    background = pygame.transform.scale(background, (width, height))
-
     start_pos = (0, (4/5)*height)
     end_pos = (width, (4/5) *height)
     line_color = (255, 255, 255)
     line_width = 3 
 
     player = Player()
-    monster_right = RightMonster()
-    monster_mid_right = MidRightMonster()
-    monster_mid = MidMonster()
-    monster_left = LeftMonster()
-    monster_mid_left = MidLeftMonster()
+    ingame_monster_list = []
+
+    # Biến thời gian
+    spawn_interval = 3000  # 3 giây = 3000 milliseconds
+    last_spawn_time = pygame.time.get_ticks()  # thời điểm spawn gần nhất
     
     running = True
     while running:
+        current_time = pygame.time.get_ticks()
+
+        if current_time - last_spawn_time >= spawn_interval:
+            new_monster = random.choice([RightMonster(),MidRightMonster(),MidMonster(),LeftMonster(),MidLeftMonster()])
+            ingame_monster_list.append(new_monster)
+            last_spawn_time = current_time
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
                 KeyHandler(event.key,player)
 
-        screen.blit(background, (0, 0))
+        screen.fill((0, 0, 0))  # nền đen
 
         pygame.draw.line(screen, line_color, start_pos, end_pos, line_width)
         player.draw(screen)
         player.update_bullets()
         
-        monster_right.draw(screen)
-        monster_right.move()
-        monster_right.auto_shoot()
-        monster_right.update_bullets()
-        
-        monster_mid_right.draw(screen)
-        monster_mid_right.move()
-        monster_mid_right.auto_shoot()
-        monster_mid_right.update_bullets()
-        
-        monster_mid.draw(screen)
-        monster_mid.move()
-        monster_mid.auto_shoot()
-        monster_mid.update_bullets()
-        
-        monster_left.draw(screen)
-        monster_left.move()
-        monster_left.auto_shoot()
-        monster_left.update_bullets()
-
-        monster_mid_left.draw(screen)
-        monster_mid_left.move()
-        monster_mid_left.auto_shoot()
-        monster_mid_left.update_bullets()
+        for monster in ingame_monster_list:
+            monster.draw(screen)
+            monster.move()
+            monster.auto_shoot()
+            monster.update_bullets()
         
         pygame.display.flip()
         clock.tick(60)
