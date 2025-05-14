c9ea4c22094d13065801e944ac8676dfd17305fd
Le Minh Duy <duylm2016@gmail.com>
2025-05-14 21:02:26 +0700

    fix: change some logic code

--- Controller/keyhandler.py
+++ Controller/keyhandler.py
@@ -8,18 +8,21 @@ def KeyHandler(command, player):
         return  # Không xử lý nếu lệnh rỗng
 
     lastWord = command.strip().split()[-1]
-    if "attack" in command and "left" in command:
-        player.direction = Direction.LEFT_PLAYER
-        player.shootBullet(Bullet, lastWord)
-    elif "attack" in command and "mleft" in command:
-        player.direction = Direction.MID_LEFT_PLAYER
-        player.shootBullet(Bullet, lastWord)
-    elif "attack" in command and "mid" in command:
-        player.direction = Direction.MID_PLAYER
-        player.shootBullet(Bullet, lastWord)
-    elif "attack" in command and "mright" in command:
-        player.direction = Direction.MID_RIGHT_PLAYER
-        player.shootBullet(Bullet, lastWord)
-    elif "attack" in command and "right" in command:
-        player.direction = Direction.RIGHT_PLAYER
-        player.shootBullet(Bullet, lastWord)
+    if "attack" in command:
+        if "mleft" in command:
+            player.direction = Direction.MID_LEFT_PLAYER
+            player.shootBullet(Bullet, lastWord)
+        elif "mright" in command:
+            player.direction = Direction.MID_RIGHT_PLAYER
+            player.shootBullet(Bullet, lastWord)
+        elif "right" in command:
+            player.direction = Direction.RIGHT_PLAYER
+            player.shootBullet(Bullet, lastWord)
+        elif "left" in command:
+            player.direction = Direction.LEFT_PLAYER
+            player.shootBullet(Bullet, lastWord)
+        elif "mid" in command:
+            player.direction = Direction.MID_PLAYER
+            player.shootBullet(Bullet, lastWord)
+    
+    
