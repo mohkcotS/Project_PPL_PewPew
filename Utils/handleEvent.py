ea5c579dadd9ff6b06b1c72a941cb1a233f69571
Le Minh Duy <duylm2016@gmail.com>
2025-05-19 15:53:58 +0700

    feat: player can collect buff fix: time monster, bullet spawn, speed of bullet

--- Utils/handleEvent.py
+++ Utils/handleEvent.py
@@ -1,12 +1,14 @@
 import pygame
 from run import runTest
 from Controller.keyhandler import KeyHandler
+from Utils.handleBuff import HandleBuff
 
-def handle_events(event, command_box, player):
+def handle_events(event, command_box, player, ingame_buff_list):
     if event.type == pygame.KEYDOWN or event.type == pygame.TEXTINPUT:
         command = command_box.handle_input(event)
         if command:
             print("Command entered:", command) 
             if(runTest(command)):
                 KeyHandler(command, player)
+                HandleBuff(command, ingame_buff_list, player)        
     return False 
